a727dadc96585c768247321cbe45745f8a349654
Vinh15088 <vinhzz1508@gmail.com>
2024-09-23 09:20:20 +0700

    init project backend

---
+++ BackEnd/SeoHotel/src/main/java/com/Seo/SeoHotel/SeoHotelApplication.java
@@ -0,0 +1,13 @@
+package com.Seo.SeoHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoHotelApplication.class, args);
+	}
+
+}
