e49ceb1c612cf1136bc58fc51e2560fb0b6875e2
Vinh15088 <vinhzz1508@gmail.com>
2024-09-24 13:06:24 +0700

    upload file security config

---
+++ BackEnd/SeoHotel/src/main/java/com/Seo/SeoHotel/security/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.Seo.SeoHotel.security;
+
+import com.Seo.SeoHotel.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
+    // create endpoints for request matcher
+    private final String[] PUBLIC_ENDPOINTS = {
+            "/auth/**",
+            "/rooms/**",
+            "/bookings/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated())
+
+                // set session management with stateless (no store state session)
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // set authentication provider
+                .authenticationProvider(authenticationProvider())
+
+                // add jwt filter before UsernamePasswordAuthenticationFileter
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    private AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        // get user's details in db (username, password)
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+
+        // encode password of user's details
+        daoAuthenticationProvider.setPasswordEncoder(passWordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    private PasswordEncoder passWordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
